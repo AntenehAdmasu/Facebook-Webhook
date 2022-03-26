34b8eb2cc95042cfc112dda7a37957244e9f9175
AntenehAdmasu <antenehadmasu8@gmail.com>
2022-03-26 11:19:54 +0300

    Clear up

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -4,19 +4,18 @@ exports.verifyToken = async (req, res) => {
 
     const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-    // Parse the query params
-    let mode = req.query['hub.mode'];
+    let hubMode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
+    let hubChallenge = req.query['hub.challenge'];
 
     // Checks if a token and mode exist in the query string
-    if (mode && token) {
+    if (hubMode && token) {
 
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            // Responds with challenge sent
-            return res.status(200).send(challenge);
+            // Respond with challenge sent
+            return res.status(200).send(hubChallenge);
         } else {
-            // Return forbidden if verify token 
+            // Return forbidden if verify token failes
             return res.sendStatus(403);
         }
     }
@@ -25,9 +24,7 @@ exports.verifyToken = async (req, res) => {
 
 exports.processRequest = async (req, res) => {
 
-    const saveReq = new Log({ requestType: "post", requestBody: JSON.stringify(req.body) });
-    await saveReq.save();
-    let body = req.body;
+    let { body } = req;
 
     // Checks if the event is from a page subscription
     if (body.object === 'page') {
@@ -38,8 +35,7 @@ exports.processRequest = async (req, res) => {
             let senderPsid = webhookEvent.sender.id;
             handleMessage(senderPsid, webhookEvent);
         });
-
-        res.status(200).send('EVENT_RECEIVED');
+        res.status(200).send('Message received');
     } else {
         res.status(400).send("Bad request");
     }
